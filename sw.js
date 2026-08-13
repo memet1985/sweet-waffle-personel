c3f880673e25228c714e21cd1c3f8cfcedc7eecc
memet1985 <memetaltun1@gmail.com>
2026-08-13 11:42:28 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,31 +1,38 @@
 self.addEventListener('push', function(event) {
-  let data = { title: "Sweet Waffle Duyuru", body: "Yeni bir mesajınız var!" };
-  
-  if (event.data) {
-    try {
-      data = event.data.json();
-    } catch (e) {
-      data.body = event.data.text();
+    let data = { title: 'Sweet Waffle - Yeni Duyuru', body: 'Merkezden yeni bir mesaj var.' };
+    if (event.data) {
+        try {
+            data = event.data.json();
+        } catch (e) {
+            data.body = event.data.text();
+        }
     }
-  }
 
-  const options = {
-    body: data.body,
-    icon: 'logo.webp',
-    badge: 'logo.webp',
-    vibrate: [200, 100, 200],
-    tag: 'sweet-waffle-notification',
-    renotify: true
-  };
+    const options = {
+        body: data.body,
+        icon: 'logo.webp',
+        badge: 'logo.webp',
+        vibrate: [200, 100, 200],
+        tag: 'sweet-waffle-broadcast',
+        renotify: true
+    };
 
-  event.waitUntil(
-    self.registration.showNotification(data.title, options)
-  );
+    event.waitUntil(
+        self.registration.showNotification(data.title, options)
+    );
 });
 
 self.addEventListener('notificationclick', function(event) {
-  event.notification.close();
-  event.waitUntil(
-    clients.openWindow('/') 
-  );
+    event.notification.close();
+    event.waitUntil(
+        clients.matchAll({ type: 'window', includeUncontrolled: true }).then(clientList => {
+            for (let i = 0; i < clientList.length; i++) {
+                let client = clientList[i];
+                if ('focus' in client) return client.focus();
+            }
+            if (clients.openWindow) {
+                return clients.openWindow('/');
+            }
+        })
+    );
 });
